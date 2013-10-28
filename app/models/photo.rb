f1f3e0df7e1b705e5cb40cb81fd5af51c946a136
Ivar Vong <ivar@ivarvong.com>
2013-10-28 14:44:37 -0700

    blah blah blah blah iptc auto blah

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,6 +4,11 @@ class Photo < ActiveRecord::Base
 	has_many :page_photos
 	has_many :photos, through: :page_photos
 
+	after_create { |record|
+		result = record.extract_iptc
+		logger.debug("after_create did extract_iptc: #{result}")		
+	}
+
 	def url_for_size(size_string)
 		if self.sizes[size_string].present?
 			self.sizes[size_string] # this is a URL. CloudFront or key?
@@ -23,8 +28,8 @@ class Photo < ActiveRecord::Base
 		}		
 		self.iptc = output_hash
 		
-		self.caption ||= self.iptc['iptc/Caption'].first
-		self.byline  ||= self.iptc['iptc/Byline'].first
+		self.caption ||= self.iptc['iptc/Caption'].try(:first) || ""
+		self.byline  ||= self.iptc['iptc/Byline'].try(:first) || ""
 
 		logger.error(self.inspect);
 
